bc05631fbf715e96ff1294640af22ec3b8fcce8a
GabMester <mestergabriel@gmail.com>
2016-02-10 17:20:01 -0500

    .

---
+++ haiku/assets/early.js
@@ -0,0 +1,6 @@
+
+$(document).ready(function(){
+    $(".first-div").click(function(){
+        $(".second-div").toggle(1000);
+         });
+});
--- haiku/assets/style.js
+++
@@ -1,3 +0,0 @@
-
-
-$('.first-div').toggleClass('red-background') 
